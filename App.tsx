ba4dfdf40d8f93a5b182aae30c7d4a4b041215c2
JStudiner <61290534+JStudiner@users.noreply.github.com>
2023-02-03 13:10:45 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import * as React from 'react'
+import React from "react"
 import { NavigationContainer } from "@react-navigation/native";
 
 import RootNavigator from "./src/navigation/RootNavigator";
